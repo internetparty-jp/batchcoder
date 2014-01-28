81aa58406334d3ab49769056cce909cae1602e6b
futoase <futoase@gmail.com>
2014-01-28 21:40:48 +0900

    Modified: Support interactive import.

--- batch.rb
+++ batch.rb
@@ -1,20 +1,23 @@
 # -*- coding:utf-8 -*-
 # How to use
 #
-# $ ruby builder.rb USERNAME PASSWORD CSVFILEPATH
-#
+# $ bundle ex ruby builder.rb -u username -p password -f csv file name
 
-require 'rubygems'
 require 'bundler/setup'
 
+require 'readline'
+require 'open-uri'
 require 'optparse'
 require 'csv'
 require 'geocoder'
 require 'faraday'
+require 'json'
 
+PREFECTURE = "東京都"
 OPTION = {}
 GTT_URL = 'http://beta.shirasete.jp/'
 PROJECT_ID = 22
+CATEGORY_URL = GTT_URL + 'projects/22/issue_categories.json'
 
 conn = Faraday::Connection.new(:url => GTT_URL) do |builder|
   builder.use Faraday::Request::UrlEncoded  # リクエストパラメータを URL エンコードする
@@ -24,7 +27,6 @@ end
 
 args = {}
 OptionParser.new do |parser|
-  parser.on('-a', '--address-prefix Address prefix') { |v| args[:address_prefix] = v}
   parser.on('-u', '--user shirasete.jp user') { |v| args[:user] = v }
   parser.on('-p', '--password shirasete.jp password') { |v| args[:password] = v }
   parser.on('-f', '--file csv file path') { |v| args[:file] = v }
@@ -32,13 +34,40 @@ OptionParser.new do |parser|
 end
 
 # ガードしとくかな...
-raise "住所がないですよ 'batch.rb -a 住所' で入力。" unless args[:address_prefix]
 raise "ユーザー名設定してー＾ー＾ 'batch.rb -u ユーザー名' " unless args[:user]
 raise "パスワード設定してー＾ー＾# 'batch.rb -u パスワード' " unless args[:password]
 raise "ファイル名指定してーーーー！ 'batch.rb -f ファイル名' " unless args[:file]
 
 user = args[:user].to_s
 password = args[:password].to_s
+
+# get categories
+categories_json = open(CATEGORY_URL, {:http_basic_authentication => [user, password]}).read
+issue_categories = JSON.parser.new(categories_json).parse['issue_categories']
+categories = {}
+issue_categories.each_with_index { |c, idx| categories[idx+1] = c["name"] }
+
+puts 'カテゴリー番号を指定してね♥'
+line_count = 0
+categories.map do |k, v|
+  print "No.#{k}: #{v}".ljust(15, ' ') 
+  line_count += 1
+  if line_count == 3
+    puts
+    line_count = 0
+  end
+end
+
+puts
+category_no = Readline.readline("No.? > ", true)
+final_category_name = categories[category_no.to_i].to_s
+
+answer = Readline.readline("#{final_category_name} で入力を始めます (y/n) ", true)
+if answer.downcase != "y"
+  puts "終了します"
+  exit
+end
+
 conn.basic_auth user, password
 
 path = File.absolute_path(args[:file].to_s)
@@ -50,8 +79,8 @@ CSV.foreach(path, :headers => true ) do |row|
   number = row[0].to_s
   number_tmp = number if number_tmp != number and number != ""
   board_name = row[1].to_s
-  address = row[2].to_s
-  subject = args[:address_prefix] + number_tmp + '-' + board_name + " " + row[3].to_s
+  address = final_category_name + row[2].to_s
+  subject = final_category_name + number_tmp + '-' + board_name + " " + row[3].to_s
   lat, lng = Geocoder.coordinates(address)
   geometry = {:type => 'Point', :coordinates => [lng, lat]}.to_json
   p geometry
